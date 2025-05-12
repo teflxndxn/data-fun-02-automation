1b305505dd2b56ad2f12413e609c349ee6d199c9
Blessing <blessing.teflon17@gmail.com>
2025-05-12 09:54:36 -0700

    Add project setup script and utils module

---
+++ bless_project_setup.py
@@ -0,0 +1,8 @@
+import utils_bless
+
+def main():
+    print("Welcome to Blessing Analytics!")
+    print(utils_bless.get_byline())  # Call the function to get the byline
+
+if __name__ == "__main__":
+    main()
